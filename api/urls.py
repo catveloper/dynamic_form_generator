a20bbb2641e9e722868487450d4608e112d8e382
donguk im <duim91@gmail.com>
2021-09-28 23:52:25 +0900

    Fix API document URL location

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from drf_spectacular.views import SpectacularJSONAPIView, SpectacularAPIView, SpectacularSwaggerView, \
+    SpectacularRedocView
 from rest_framework import routers
 
 from api.views import *
@@ -9,10 +11,21 @@ router = routers.DefaultRouter()
 router.register(r'users', viewset=UserViewSet)
 router.register(r'groups', viewset=GroupViewSet)
 
+# Auto Generate API
 urlpatterns = [
-    # Auto Generate API
     path('', include(router.urls)),
+]
 
-    # Custom API
+# Custom API
+urlpatterns += [
     path('form_generate/static/', StaticFormGeneratorAPI.as_view(), name='static_form_schema'),
+]
+
+# Spectacular Document API
+urlpatterns += [
+    path("docs/json/", SpectacularJSONAPIView.as_view(), name="schema-json"),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/user', SpectacularAPIView.as_view(), name='user_schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='api:schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='api:schema'), name='redoc'),
 ]
